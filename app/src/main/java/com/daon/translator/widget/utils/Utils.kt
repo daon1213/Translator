3d7df872c53826a6687c8c88e09bcca0c172878a
daon1213 <diaeiyo1213@naver.com>
2023-04-01 13:32:22 +0900

    🔧 :: PAPAGO API

---
+++ app/src/main/java/com/daon/translator/widget/utils/Utils.kt
@@ -0,0 +1,11 @@
+package com.daon.translator.widget.utils
+
+abstract class Utils {
+
+    companion object {
+        const val X_NAVER_CLIENT_ID = "6xUMZH_EG9kfjJdrsvA2"
+        const val X_NAVER_CLIENT_SECRET = "OF_vGgOnqo"
+        const val BASE_URL = "https://openapi.naver.com"
+    }
+
+}
